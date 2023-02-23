0d311c951638ca46c28bcf9499f1a7bb18e37ef8
Mario Abdala <mario_abdala@hotmail.com>
2023-02-23 12:34:55 -0300

    Control props alternativo

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -5,17 +5,17 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({});
     const onProductCountChange = ({count, product}: {count: number, product: Product}) => {
         setShoppingCart(oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count+= count;
-                return {...oldShoppingCart, [product.id]: productInCart}
-            };
-            const {[product.id]: _, ...rest} = oldShoppingCart;
-            return rest;
+            if (count === 0) {
+              const {[product.id]: _, ...rest} = oldShoppingCart;
+              return rest;
+            }
+            return {
+              ...oldShoppingCart, [product.id]: {...product, count}
+            }
         });
     };
   return {
     shoppingCart, 
     onProductCountChange
   };
-}
+};
