524d325957ec2575bf53a62a87457c4654483fad
Mario Abdala <mario_abdala@hotmail.com>
2023-02-28 17:32:23 -0300

    Small fixes pre NPM

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductButtons, ProductCard, ProductTitle, ProductImage } from '../components';
 import { products } from '../data';
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -9,13 +8,13 @@ export const ShoppingPage = () => {
     <div>
         <h1>Shopping Store</h1>
         <hr />
-            <ProductCard key={product.id} product={product} className="bg-dark text-white" initialValues={{count: 4, maxCount: 10}}>
+            <ProductCard key={product.id} product={product} initialValues={{count: 4, maxCount: 10}}>
                 {
                     ({}) => (
                         <>
-                            <ProductImage className="custom-image" />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                         )
                 }
